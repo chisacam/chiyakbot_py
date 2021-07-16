f231ed59c095297a009080fd6cbb26e8e2792d83
chisacam <dladsds123@gmail.com>
2021-07-16 13:41:39 +0900

    replykeyboardmarkup for specific user

--- reminder.py
+++ reminder.py
@@ -14,7 +14,8 @@ import escape
 chiyak = chatbotmodel.chiyakbot()
 file_path = './reminded.json'
 alert_users = []
-is_only_time = re.compile('^((202[0-9]{1})(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01]))?((0[0-9]|1[0-9]|2[0-3])([0-5][0-9])){1}$')
+is_only_time = re.compile(
+    '^((202[0-9]{1})(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01]))?((0[0-9]|1[0-9]|2[0-3])([0-5][0-9])){1}$')
 DREMIND = 0
 if os.path.exists(file_path):
     with open(file_path, "r") as json_file:
@@ -29,7 +30,8 @@ class Worker(threading.Thread):
     def run(self):
         while(True):
             for remind_task in alert_users[:]:
-                now = datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y%m%d%H%M')
+                now = datetime.datetime.now(
+                    timezone('Asia/Seoul')).strftime('%Y%m%d%H%M')
                 if now >= remind_task['remind_date']:
                     chiyak.core.sendMessage(chat_id=remind_task['remind_chat_id'], reply_to_message_id=remind_task['remind_message_id'], text='다시 확인해보실 시간이에요!\n메모: {}'.format(
                         remind_task['remind_text']))
@@ -57,20 +59,24 @@ def reminder_register(update, context):
     if update.message.reply_to_message is not None:
         if update.message.reply_to_message.text is not None:
             if len(user_input) > 1:
-                result = regist(chat_id, user_id, update.message.reply_to_message.text, remind_text_id, user_input[1].strip(), 'reply')
+                result = regist(chat_id, user_id, update.message.reply_to_message.text,
+                                remind_text_id, user_input[1].strip(), 'reply')
                 update.message.reply_text(result['message'])
             else:
-                update.message.reply_text('언제 다시 알려드릴지 모르겠어요. 입력을 다시 확인해주세요!\n/remind [날짜] [리마인드할문자열]\n또는\n리마인드할 메세지에 답장후 /remind [날짜]')
+                update.message.reply_text(
+                    '언제 다시 알려드릴지 모르겠어요. 입력을 다시 확인해주세요!\n/remind [날짜] [리마인드할문자열]\n또는\n리마인드할 메세지에 답장후 /remind [날짜]')
                 return
         else:
             update.message.reply_text('텍스트를 찾을 수 없어요!')
             return
     else:
         if len(user_input) > 2:
-            result = regist(chat_id, user_id, user_input[2].strip(), remind_text_id, user_input[1].strip(), 'param')    
+            result = regist(chat_id, user_id, user_input[2].strip(
+            ), remind_text_id, user_input[1].strip(), 'param')
             update.message.reply_text(result['message'])
         else:
-            update.message.reply_text('다시 알려드릴 시간이나 메세지를 못찾겠어요. 입력을 다시 확인해주세요!\n/remind [날짜] [리마인드할문자열]\n또는\n리마인드할 메세지에 답장후 /remind [날짜]')
+            update.message.reply_text(
+                '다시 알려드릴 시간이나 메세지를 못찾겠어요. 입력을 다시 확인해주세요!\n/remind [날짜] [리마인드할문자열]\n또는\n리마인드할 메세지에 답장후 /remind [날짜]')
             return
     with open(file_path, 'w') as outfile:
         json.dump(alert_users, outfile, indent=4, ensure_ascii=False)
@@ -79,23 +85,26 @@ def reminder_register(update, context):
 def reminder_delete_command(update, context):
     user_id = update.message.from_user.id
     chat_id = update.message.chat_id
-    registered_work = [[KeyboardButton(text='{}\n\n{}'.format(item['remind_text'],item['remind_uuid']))] for item in alert_users if item['reminder_user_id'] == user_id]
+    registered_work = [[KeyboardButton(text='{}\n\n{}'.format(item['remind_text'], item['remind_uuid']))]
+                       for item in alert_users if item['reminder_user_id'] == user_id]
     if len(registered_work) == 0:
         chiyak.core.sendMessage(chat_id=chat_id, text='삭제할 리마인더가 없어요!')
         return ConversationHandler.END
     registered_work.append([KeyboardButton(text='선택종료')])
-    update.message.reply_text('취소할 리마인더를 선택해주세요!', reply_markup=ReplyKeyboardMarkup(keyboard=registered_work, resize_keyboard=True))
+    update.message.reply_text('취소할 리마인더를 선택해주세요!', reply_markup=ReplyKeyboardMarkup(
+        keyboard=registered_work, resize_keyboard=True, selective=True))
     return DREMIND
 
 
 def remind_delete_error(update, context):
-#    with open(file_path, 'w') as outfile:
-#        json.dump(alert_users, outfile, indent=4, ensure_ascii=False)
-#        chiyak.sendMessage(update.message.chat_id, '해제했어요!')
+    #    with open(file_path, 'w') as outfile:
+    #        json.dump(alert_users, outfile, indent=4, ensure_ascii=False)
+    #        chiyak.sendMessage(update.message.chat_id, '해제했어요!')
     update.message.reply_text(
         '에러 또는 선생님의 선택으로 취소됐어요!', reply_markup=ReplyKeyboardRemove()
     )
     return ConversationHandler.END
+
 
 def remind_delete(update, context):
     token = update.message.text.split('\n')
@@ -110,9 +119,11 @@ def remind_delete(update, context):
     else:
         for remind_task in alert_users[:]:
             if token[2] == remind_task['remind_uuid']:
-                chiyak.core.sendMessage(chat_id=remind_task['remind_chat_id'], text='리마인더 {}를 선택했어요!'.format(remind_task['remind_text']))
+                chiyak.core.sendMessage(chat_id=remind_task['remind_chat_id'], text='리마인더 {}를 선택했어요!'.format(
+                    remind_task['remind_text']))
                 alert_users.remove(remind_task)
         return DREMIND
+
 
 def regist(chat_id, user_id, remind_text, remind_text_id, user_date, type):
     result = is_correct_time(user_date)
@@ -131,6 +142,7 @@ def regist(chat_id, user_id, remind_text, remind_text_id, user_date, type):
 
     return result
 
+
 def is_correct_time(user_date):
     default_date = datetime.datetime.now(
         timezone('Asia/Seoul')).strftime('%Y%m%d')
@@ -140,17 +152,18 @@ def is_correct_time(user_date):
         is_correct['result'] = True
         is_correct['date'] = fin_date
         is_correct['message'] = '{}에 다시 알려드릴게요!'.format(
-                    datetime.datetime.strptime(fin_date, "%Y%m%d%H%M"))
+            datetime.datetime.strptime(fin_date, "%Y%m%d%H%M"))
     else:
         is_correct['result'] = False
         is_correct['message'] = '저런, 날짜형식에 맞지 않는것같아요! 아래 예시를 참고해주세요!\nYYYYmmddHHMM(예시: 202107071800)\n또는\nHHMM(예시: 1800)'
 
     return is_correct
 
+
 rm_remind_handler = ConversationHandler(
-        entry_points=[CommandHandler('dremind', reminder_delete_command)],
-        states={
-            DREMIND: [MessageHandler(Filters.text, remind_delete)]
-        },
-        fallbacks=[CommandHandler('drcancel', remind_delete_error)],
-    )
+    entry_points=[CommandHandler('dremind', reminder_delete_command)],
+    states={
+        DREMIND: [MessageHandler(Filters.text, remind_delete)]
+    },
+    fallbacks=[CommandHandler('drcancel', remind_delete_error)],
+)
