2c664e3700c2069711379e1a341ea302cedc3e53
chisacam <dladsds123@gmail.com>
2021-09-14 19:10:15 +0900

    more summary extract for response

--- lib/namusearch.py
+++ lib/namusearch.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3.dbapi2 import connect
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from uuid import uuid4
 
@@ -13,11 +14,14 @@ def inlinequeryhandler(update, context):
     result = sql.fetchall()
     results = []
     for title, content in result:
+        lines = content.split('.')
+        get_line_limit = 5 if len(lines) >= 5 else len(lines)
+        summary = ''.join(lines[:get_line_limit])
         results.append(InlineQueryResultArticle(
             id=str(uuid4()),
             title=title,
-            input_message_content=InputTextMessageContent(f'/namu {title}'),
-            description=content
+            input_message_content=InputTextMessageContent(summary),
+            description=summary
         ))
     update.inline_query.answer(results)
     sql.close()
@@ -26,6 +30,13 @@ def search_namu(query):
     conn = sqlite3.connect('./namu.db')
     sql = conn.cursor()
     sql.execute(f"SELECT summary FROM namu WHERE title='{query}'")
-    result = sql.fetchone()
+    content = sql.fetchone()
     sql.close()
-    return result[0] if result != None else '검색결과가 없어요!'
+    result = ''
+    if content != None:
+        lines = content[0].split('.')
+        get_line_limit = 5 if len(lines) >= 5 else len(lines)
+        result = ''.join(lines[:get_line_limit])
+    else:
+        result = '검색결과가 없어요!'
+    return result
