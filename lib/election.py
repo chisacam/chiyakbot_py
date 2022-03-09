28bcccdf6154e2353e76f3249ad846547ca64a0e
chisacam <dladsds123@gmail.com>
2022-03-09 22:10:09 +0900

    add percent

--- lib/election.py
+++ lib/election.py
@@ -5,8 +5,8 @@ def getElectionStatus():
     res = requests.get('https://bit.ly/2022-Korean-presidential-election')
     soup = BeautifulSoup(res.text, 'html.parser')
     table = soup.select_one('#table01')
-    the_min_name = table.select_one('tbody > tr:nth-child(1) > td:nth-child(4) > strong').get_text(' ', strip=True)
-    gook_gim_name = table.select_one('tbody > tr:nth-child(1) > td:nth-child(5) > strong').get_text(' ', strip=True)
+    the_min_name = table.select_one('tbody > tr:nth-child(1) > td:nth-child(4) > strong').get_text(' ', strip=True).split(' ')[1]
+    gook_gim_name = table.select_one('tbody > tr:nth-child(1) > td:nth-child(5) > strong').get_text(' ', strip=True).split(' ')[1]
     the_min_per = table.select_one('tbody > tr:nth-child(3) > td:nth-child(4)').get_text()
     gook_gim_per = table.select_one('tbody > tr:nth-child(3) > td:nth-child(5)').get_text()
-    return f'{the_min_name} {the_min_per}', f'{gook_gim_name} {gook_gim_per}'
+    return f'{the_min_name} {the_min_per}%', f'{gook_gim_name} {gook_gim_per}%'
