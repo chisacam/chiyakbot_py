54c0b3d89bbf3a8c3fc870231dc18004a5105caa
chisacam <dladsds123@gmail.com>
2022-03-09 22:08:15 +0900

    remove trash

--- lib/election.py
+++ lib/election.py
@@ -9,8 +9,4 @@ def getElectionStatus():
     gook_gim_name = table.select_one('tbody > tr:nth-child(1) > td:nth-child(5) > strong').get_text(' ', strip=True)
     the_min_per = table.select_one('tbody > tr:nth-child(3) > td:nth-child(4)').get_text()
     gook_gim_per = table.select_one('tbody > tr:nth-child(3) > td:nth-child(5)').get_text()
-    print(the_min_name, the_min_per)
-    print(gook_gim_name, gook_gim_per)
     return f'{the_min_name} {the_min_per}', f'{gook_gim_name} {gook_gim_per}'
-
-getElectionStatus()
