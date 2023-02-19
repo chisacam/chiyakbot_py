faafccf769cc5b4bf2a6dbee69ae95d1cd81c586
Kyujin Cho <kyujin.cho@lablup.com>
2023-02-20 01:17:17 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git pull
 kill $(ps -ef | grep chiyakbot.py | gawk '/python3/ { print $2 }')
 sleep 5
-nohup python3 -u chiyakbot.py production > chiyakbot.log &
+nohup python -m chiyakbot.bot production > chiyakbot.log &
