d43912b30bc2c853fe569934072f037c599e09d2
chisacam <dladsds123@gmail.com>
2025-05-06 16:51:28 +0900

    fix: 네이버 환율 쓰는걸로 변경

--- src/chiyakbot/chatbots/exchange.py
+++ src/chiyakbot/chatbots/exchange.py
@@ -18,27 +18,19 @@ top_code = ["USD","CNY","JPY(100)","EUR","HKD","TWD","GBP","VND","CAD","RUB"]
 class ExchangeModel(AbstractChatbotModel):
     name = "Exchange"
     api_key = os.getenv("EXCHANGE_API_KEY")
-    async def request_info(self, req_code="USD") -> Mapping[str, Any]:
-        match req_code:
-            case "ALL":
-                req = all_code
-            case "TOP":
-                req = top_code
-            case _:
-                req = req_code
+    async def request_info(self, req_code="USD", amount=1) -> Mapping[str, Any]:
 
         async with httpx.AsyncClient() as client:
             response = await client.get(
-                f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={self.api_key}&data=AP01"
+                f"https://m.search.naver.com/p/csearch/content/qapirender.nhn?key=calculator&pkid=141&q=환율&where=m&u1=keb&u6=standardUnit&u7=0&u3={req_code}&u3=USD&u4=KRW&u8=down&u2={amount}"
             )
-            # print(response.status_code)
             if response.status_code == 200:
                 dict_response = response.json()
                 if dict_response != []:
                     return {
                         "result": True,
                         "message": "요청을 성공했어요!",
-                        "data": dict_response,
+                        "data": dict_response["country"],
                     }
                 else:
                     return {
@@ -61,12 +53,10 @@ class ExchangeModel(AbstractChatbotModel):
             ),
         ]
 
-    def get_target_exchange_data(self, exchange_data, target_code):
+    def get_target_exchange_data(self, exchange_data):
         target_data = []
         for item in exchange_data:
-            if item["cur_unit"].replace("(100)", "") in target_code:
-                item["cur_unit"] = item["cur_unit"].replace("(100)", "")
-                target_data.append(item)
+            target_data.append(item["subValue"])
         return target_data
 
     async def get_exchange_command(
@@ -75,70 +65,47 @@ class ExchangeModel(AbstractChatbotModel):
         result = ""
         input_code = "USD"
         user_input = (message.text or "").split(" ", 1)
-        table = prettytable.PrettyTable(["CODE", "BASE", "BUY", "SELL"])
+        table = prettytable.PrettyTable(["CODE", "BASE"])
         table.align["CODE"] = "l"
         table.align["BASE"] = "r"
-        table.align["BUY"] = "r"
-        table.align["SELL"] = "r"
         if len(user_input) > 1:
             input_code = user_input[1].upper()
-        exchange_data = await self.request_info()
+        exchange_data = await self.request_info(input_code)
         target_data = []
         if exchange_data["result"]:
             now = datetime.now()
-            target_data = self.get_target_exchange_data(exchange_data["data"], input_code)
+            target_data = exchange_data["data"]
             result += "date: " + now.date().strftime("%Y%m%d") + "\n"
             result += "time: " + now.timetz().strftime("%H:%M:%S") + "\n"
             for item in target_data:
-                # print(item)
                 table.add_row(
                     [
-                        item["cur_unit"],
-                        item["deal_bas_r"],
-                        item["ttb"],
-                        item["tts"],
+                        item["currencyUnit"],
+                        item["value"],
                     ]
                 )
             result += table.get_string()
         else:
             message = exchange_data["message"]
             await message.reply_text(f"{message}")
-        im = Image.new("RGB", (250, 110 + len(target_data) * 19), "white")
-        draw = ImageDraw.Draw(im)
-        font = ImageFont.truetype("./JetBrainsMonoNL-Regular.ttf", 13)
-        draw.text((10, 10), result, font=font, fill="black")
-        im_byte = io.BytesIO()
-        im.save(im_byte, format="png", bitmap_format="png")
-        await self.bot.send_photo(message.chat.id, im_byte.getvalue())
-        im.close()
-        im_byte.close()
+        await message.reply_text(result)
 
     async def calc_exchange_command(
         self, update: Update, message: Message, context: ContextTypes.DEFAULT_TYPE
     ):
-        result = None
         user_input = (message.text or "").split(" ")
         if len(user_input) <= 2:
             await message.reply_text("뭔가 빠진거같아요! 다시 시도해주세요!")
         else:
             input_code = user_input[1].upper()
-            exchange_data = await self.request_info()
             input_cur = round(float(user_input[2].replace(",", "")), 2)
-            format_cur = format(input_cur, ",")
+            exchange_data = await self.request_info(input_code, input_cur)
             if exchange_data["result"]:
                 try:
-                    item = self.get_target_exchange_data(exchange_data["data"], input_code)[0]
-                    result = format(
-                        round(
-                            float(item["deal_bas_r"].replace(",", ""))
-                            * input_cur
-                            / (100 if item["cur_unit"] == "JPY" else 1),
-                            2,
-                        ),
-                        ",",
-                    )
+                    item = self.get_target_exchange_data(exchange_data["data"])
+                    print(item)
                     await message.reply_text(
-                        f"{format_cur} {input_code} ≈ {result} KRW"
+                        f"{item[0]} ≈ {item[1]}"
                     )
                 except:
                     await message.reply_text(
